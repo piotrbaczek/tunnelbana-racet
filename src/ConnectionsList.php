69cc2ce9a05a4f63f771077dc17efc91adeaeeee
Piotr Bączek <pgolasz@gmail.com>
2021-12-27 15:21:36 +0100

    working test

--- src/ConnectionsList.php
+++ src/ConnectionsList.php
@@ -2,43 +2,15 @@
 
 namespace pbaczek\tunnelbanarace;
 
-use pbaczek\tunnelbanarace\Stations\AbstractStation;
+use Ramsey\Collection\AbstractCollection;
 
-class ConnectionsList
+class ConnectionsList extends AbstractCollection
 {
-    /** @var Connection[] */
-    private $connections = [];
-
     /**
-     * @param AbstractStation $abstractStation
-     * @param int $timeInMinutes
-     * @return $this
+     * Returns the type associated with this collection.
      */
-    public function connectsTo(AbstractStation $abstractStation, int $timeInMinutes): self
+    public function getType(): string
     {
-        $this->connections[] = new Connection($abstractStation, $timeInMinutes);
-
-        return $this;
-    }
-
-    /**
-     * Get connection by name
-     * @param string $name
-     * @return Connection|null
-     */
-    public function getConnectionByName(string $name): ?Connection
-    {
-        foreach ($this->connections as $connection) {
-            if ($connection->getAbstractStation()->getName() === $name) {
-                return $connection;
-            }
-        }
-
-        return null;
-    }
-
-    public function getConnections()
-    {
-        return $this->connections;
+        return Connection::class;
     }
 }
